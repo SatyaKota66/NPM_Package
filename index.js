982ed9c4c0e5f42ce1c257be0e9ef307e8692ca0
Satyavathi Kota <satyavathi.kota@pal.tech>
2025-07-30 18:16:44 +0530

    Uploading a package

---
+++ index.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    console.log("Adding of 2 numbers");
+    return a+b;
+}
+function subtract(a,b){
+    console.log("Subtracting of 2 numbers");
+    return a-b;
+}
+module.exports={add,subtract};
